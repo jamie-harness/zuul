27c507e4803ad3536b21c537d783b960ea9acf26
Jamie Li <tianyi.li@harness.io>
2024-11-25 12:17:16 -0500

    rename jenkins utils methods

--- gradleUtils.groovy
+++ gradleUtils.groovy
@@ -1,8 +1,12 @@
-// Methods in this file will end up as object methods on the object that load returns.
 def gradlecleantest() {
     sh "echo 'starting unit test with gradle'"
     sh "./gradlew clean test --continue"
     junit "**/test/TEST*.xml"
+}
+
+def gradlebuild() {
+    sh "echo 'starting build with gradle'"
+    sh "./gradlew build"
 }
 
 return this;
