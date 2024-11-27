189de29cb4a3c06de7b2c168383d50d4d847ed2b
Jamie Li <tianyi.li@harness.io>
2024-11-27 12:26:40 -0500

    Update dockerUtils.groovy

--- dockerUtils.groovy
+++ dockerUtils.groovy
@@ -1,6 +1,9 @@
 // If there's a call method, you can just load the file, say, as "foo", and then invoke that call method with foo(...) 
 def buildAndPush(String username) {
     sh "docker login -u ${username} -p \$DOCKER_PASSWORD"
+    def customImage = docker.build("username/zuul:latest")
+    customImage.push()
+    return customImage.name
 }
 
 return this;
